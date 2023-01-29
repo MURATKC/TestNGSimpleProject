82338b4190a710ccb06041fb0810b99e775c139e
muti <ares990@gmail.com>
2023-01-29 05:30:41 -0700

    this is for fifth

---
+++ src/test/java/com/ohrm/login/TC_3.java
@@ -0,0 +1,8 @@
+package com.ohrm.login;
+
+public class TC_3 {
+	public static void main(String[] args) {
+		System.out.println("this is test case 3");
+	}
+
+}
--- src/test/java/com/ohrm/utilities/Driver.java
+++ src/test/java/com/ohrm/utilities/Driver.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Driver {
-    private Driver(){
+    private Dri ver(){
     }
      private static WebDriver driver;
 
