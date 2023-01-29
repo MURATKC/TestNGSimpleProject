56ffe551deb24b7c7810d858321744448b0c5157
muti <ares990@gmail.com>
2023-01-29 06:01:07 -0700

    last version added

--- src/test/java/com/ohrm/login/TC_3.java
+++ src/test/java/com/ohrm/login/TC_3.java
@@ -4,6 +4,7 @@ public class TC_3 {
 	public static void main(String[] args) {
 		System.out.println("this is test case 3");
 		System.out.println("duzeltme");
+		System.out.println("anpther duzeltme");
 	}
 
 }
---
+++ src/test/java/com/ohrm/login/TC_SERACHCUSTOMER.java
@@ -0,0 +1,7 @@
+package com.ohrm.login;
+
+public class TC_SERACHCUSTOMER {
+public static void main(String[] args) {
+	System.out.println("search this customer");
+}
+}
